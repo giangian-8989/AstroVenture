18ac733cd862922f5c0cb0db3a120009ad4c8a02
giangian-8989 <bgiancarlo892@gmail.com>
2025-08-29 01:17:13 +0000

    bernard

---
+++ javascript/quiz.js
@@ -0,0 +1,11 @@
+function toSelectDifficulty(){
+    window.location.href = "../html/selectDifficulty.html";
+}
+function popCorrect(){
+    alert("Correct");
+}
+function toEasyGame(){
+    window.location.href = "../html/easy.html";
+}
+
+window.popCorrect = popCorrect;
--- javascript/settings.js
+++ javascript/settings.js
@@ -1,16 +1,62 @@
-let ismechanicsVisible = false;
-let isaboutVisible = false;
+// let ismechanicsVisible = false;
+// let isaboutVisible = false;
+window.onload = function(){
 
+let aboutContent = document.getElementById("about-content");
+let aboutHeader = document.getElementById("about-header");
+let mechanicsContent = document.getElementById("mechanics-content");
+let mechanicsHeader = document.getElementById("mechanics-header");
+let controlsContent = document.getElementById("controls-content");
+let controlsHeader = document.getElementById("controls-header");
+let volumesContent = document.getElementById("volumes-content");
+let volumesHeader = document.getElementById("volumes-header");
+
+console.log("aboutContent:", aboutContent);
+console.log("aboutHeader:", aboutHeader);
+console.log("mechanicsContent:", mechanicsContent);
+console.log("mechanicsHeader:", mechanicsHeader);
+console.log("controlsContent:", controlsContent);
+console.log("controlsHeader:", controlsHeader);
+console.log("volumeContent:", volumeContent);
+console.log("volumeHeader:", volumeHeader);
+
+
+
+
+
+};
 function show(){
-    let element = document.getElementById("about");
-    let element2 = document.getElementById("about-header");
-    element2.classList.toggle("hidden")
-    element.classList.toggle("hidden");
+    try{
+    aboutContent.classList.remove("hidden");
+    aboutHeader.classList.toggle("hidden");
+    } catch{
+        console.error(TypeError);
     }
+}
 
 function showMechanics(){
-    let element = document.getElementById("mechanics");
-    let element2 = document.getElementById("mechanics-header");
-    element2.classList.toggle("hidden")
-    element.classList.toggle("hidden");
+    clearInterface();
+    mechanicsHeader.classList.remove("hidden")
+    mechanicsContent.classList.remove("hidden");
+}
+function showControls(){
+    clearInterface();
+    controlsContent.classList.remove("hidden");
+    controlsHeader.classList.remove("hidden");
+}
+function showVolumes(){
+    clearInterface();
+    volumesContent.classList.remove("hidden");
+    volumesHeader.classList.remove("hidden");
+}
+
+function clearInterface(){
+    aboutContent.classList.add("hidden");
+    aboutHeader.classList.add("hidden");
+    mechanicsContent.classList.add("hidden");
+    mechanicsHeader.classList.add("hidden");
+    controlsContent.classList.add("hidden");
+    controlsHeader.classList.add("hidden");
+    volumesContent.classList.add("hidden");
+    volumesHeader.classList.add("hidden");
 }
